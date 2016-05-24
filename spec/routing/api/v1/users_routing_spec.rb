bccb44a82273585f1d42f6729d7b630f3060f320
nchristiny <icarus747@gmail.com>
2016-05-24 14:06:53 -0500

    Pass routing spec

--- spec/routing/api/v1/users_routing_spec.rb
+++ spec/routing/api/v1/users_routing_spec.rb
@@ -3,32 +3,28 @@ require "rails_helper"
 RSpec.describe Api::V1::UsersController, :type => :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/users").to route_to("users#index")
+    it 'routes to #index' do
+      expect(:get => '/users').to route_to(controller: 'api/v1/users', action: 'index', format: :json)
     end
 
-    it "routes to #new" do
-      expect(:get => "/users/new").to route_to("users#new")
+    it 'routes to #new' do
+      expect(:get => '/users/new').to route_to(controller: 'api/v1/users', action: 'new', format: :json)
     end
 
-    it "routes to #show" do
-      expect(:get => "/users/1").to route_to("users#show", :id => "1")
+    it 'routes to #show' do
+      expect(:get => '/users/1').to route_to(controller: 'api/v1/users', action: 'show', id: '1', format: :json)
     end
 
-    it "routes to #edit" do
-      expect(:get => "/users/1/edit").to route_to("users#edit", :id => "1")
+    it 'routes to #create' do
+      expect(:post => '/users').to route_to(controller: 'api/v1/users', action: 'create', format: :json)
     end
 
-    it "routes to #create" do
-      expect(:post => "/users").to route_to("users#create")
+    it 'routes to #update' do
+      expect(:put => '/users/1').to route_to(controller: 'api/v1/users', action: 'update', id: '1', format: :json)
     end
 
-    it "routes to #update" do
-      expect(:put => "/users/1").to route_to("users#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/users/1").to route_to("users#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(:delete => '/users/1').to route_to(controller: 'api/v1/users', action: 'destroy', id: '1', format: :json)
     end
 
   end
