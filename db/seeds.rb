30e931260f80a91103f4205f46f42d5fe5707297
nchristiny <icarus747@gmail.com>
2016-06-04 21:53:17 -0500

    Add JSON data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,22 @@ require 'json'
 
 ## CARDS
 
+card_array = File.readlines("lib/assets/reward.json").each {|line| line.chomp!}
 
-
+card_array.each do |card|
+  card_json = JSON.parse(card)
+  Card.create!(card_json)
+  p "********************************************************************************"
+end
+cards = Card.all
+p "There are #{cards.count} cards in the database presently."
 
 ## USERS
 
-5.times do
-  user = User.create!({ name: FFaker::Internet.user_name, email: FFaker::Internet.disposable_email, password: FFaker::Internet.password })
-  p "Created user: " + user.id.to_s + " - " + user.name + " - " + user.email + " - " + user.api_key
-end
+# 5.times do
+#   user = User.create!({ name: FFaker::Internet.user_name, email: FFaker::Internet.disposable_email, password: FFaker::Internet.password })
+#   p "Created user: " + user.id.to_s + " - " + user.name + " - " + user.email + " - " + user.api_key
+# end
 
 # cURL commands
 # Get user page
