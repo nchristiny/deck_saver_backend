279522c7886be160d0df79140df2c991fe2365d9
nchristiny <icarus747@gmail.com>
2016-06-07 19:48:22 -0500

    Add spec deck and card controllers

--- spec/controllers/api/v1/decks_controller_spec.rb
+++ spec/controllers/api/v1/decks_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe DecksController, type: :controller do
-
+RSpec.describe Api::V1::DecksController, type: :controller do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
