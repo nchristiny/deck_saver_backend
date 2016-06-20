0bc52c6e3e7037bb4a745c3946c689ad58b27c5f
nchristiny <icarus747@gmail.com>
2016-06-20 18:55:04 -0500

    Add inverse_of to deck_cards associations

--- app/models/deck_card.rb
+++ app/models/deck_card.rb
@@ -2,6 +2,6 @@ class DeckCard < ApplicationRecord
   validates :card_id, presence: true
   validates :deck_id, presence: true
 
-  belongs_to :deck
-  belongs_to :card
+  belongs_to :deck, inverse_of: :deck_cards
+  belongs_to :card, inverse_of: :deck_cards
 end
